3cd12ee99c95f6e76f533d780ce7f683dec0b122
Annie Vang <akvang@gmail.com>
2011-09-18 22:07:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookit::Application.config.secret_token = '437a8b51c115651c055010293f2566db59e5adb96ba984a052c81636309b9e462752c40e9d5beeb4a3af0a2e936c0799927b6593165504c36b1f4514ec8bf69f'
