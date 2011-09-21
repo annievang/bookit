2c6c9709f16e8158df21e47b74e681d139a0b26a
Annie Vang <akvang@gmail.com>
2011-09-20 23:19:56 -0500

    edited to push

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Bookit::Application.routes.draw do
   resources :bookmarks
   root :to => 'bookmarks#index'
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
